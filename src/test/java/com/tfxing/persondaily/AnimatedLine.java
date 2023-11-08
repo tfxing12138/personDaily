74d9ddc95ccf8259442441219d75a9a5877cc911
link <t18370240624@163.com>
2023-11-08 20:09:45 +0800

    javaswing

--- src/test/java/com/tfxing/persondaily/AnimatedLine.java
+++ src/test/java/com/tfxing/persondaily/AnimatedLine.java
@@ -8,11 +8,16 @@ import java.awt.event.ActionListener;
 public class AnimatedLine extends JPanel implements ActionListener {
     private int x1, y1, x2, y2;
 
+    private double angle;
+
     public AnimatedLine() {
         x1 = 100;
         y1 = 10;
         x2 = 200;
         y2 = 10;
+
+        angle = 30;
+
         Timer timer = new Timer(50, this); // 创建定时器，每隔50毫秒触发一次
         timer.start();
     }
@@ -20,28 +25,39 @@ public class AnimatedLine extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawLine(x1, y1, x2, y2);
+        g.drawLine(x1, y1*10, x2, y2*10);
+
+        g.drawLine(x1, y1, x1, y1*10);
+        g.drawLine(x2, y2, x2, y2*10);
+
+        double length = (x2-x1)*Math.cos(Math.toRadians(angle));
+
+        double hight = (y2 * 10 - y1) * Math.sin(Math.toRadians(angle / 2));
+        double hightTop = (y2 * 10 + y1) + hight / 2;
+        double hightButtom = (y2 * 10 + y1) - hight / 2;
+
+        g.drawLine((int)(x1+length), (int) hightTop, (int)(x1+length), (int) hightButtom);
+
+
     }
 
     public void actionPerformed(ActionEvent e) {
-        // 更新线条的位置
-        y1++;
-        y2++;
-        if (x2 > getWidth() || y2 > getHeight()) {
-            x1 = 10;
-            y1 = 10;
-            x2 = 100;
-            y2 = 10;
+        // 更新角度大小
+        if(angle < 90) {
+            angle++;
+        } else if(angle >= 90) {
+            angle = 0;
         }
+
         repaint(); // 重绘线条
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Animated Line");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
         frame.add(new AnimatedLine());
         frame.setVisible(true);
 
-        frame.setBounds(400,300,400, 300);
+        frame.setBounds(500,500,400, 400);
     }
 }
